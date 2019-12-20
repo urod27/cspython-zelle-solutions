6b7e222e29b5d284355473fdd33d354be0696041
Ulises Rodriguez <39100176+urod27@users.noreply.github.com>
2019-12-19 20:58:56 -0600

    Near completion
    Signed-off-by: urod27 <rodriuli@oregonstate.edu>

---
+++ CH1.py
@@ -0,0 +1,81 @@
+# Review questions
+
+# 1. Computer science is not the study of computers. Computers are simply a tool to design, analyze, and experiment with
+#       information.
+#
+# 2. The Central Processing Unit is the brain of the computer. The CPU accesses information that is stored in the main
+#       memory (this main memory is called Random Access Memory). Input devices such as a keyboard or monitor is
+#       processed also by the CPU. The CPU follows a "fetch-execute cycle" for processing: fetch -> decode -> execute.
+#
+# 3. Secondary memory on a personal computer is usually either an internal hard disk drive (HDD) or
+#       solid disk drive (SSD). HHD uses magnetic patterns on a spinning disk and SSD uses flash memory.
+#
+# 4. When a program is started the following happens in order:
+#       1) A copy from secondary memory is made to the main memory (RAM)
+#       2) CPU executes the program once it is loaded
+#
+# 5. SEMANTICS refers to "precise meaning". SYNTAX refers to "precise form" or structure. Semantics comes after syntax.
+#       As an example using modern English: "Bill is jumping while sitting by the fire place.", is syntactically
+#       correct, but not semantically (no meaning).
+#
+# 6. A function executes a specific task. It takes data in and returns a result after processing.
+#
+# 7. A programming environment refers to an IDE. The one I like to use is PyCharm.
+#
+# 8. A variable in Python works like in a math class. Variables are ASSIGNED to data (assignment). The best way to
+#       think of it in Python is as a "sticky-note". For example, a variable named "coolio" acts like a sticky note for
+#       some data that I want to assign such that
+#       coolio = 1 * 4
+#
+# 9. A loop is for repeating or iterating an operation. Best way to think of it is in terms of graph theory, where an
+#       edge is connected to a vertex to itself.
+#
+# 10. Chaos theory as it relates to computer science: some dynamic systems are irregular, unpredictable, and very very
+#       sensitive to initial values, and computers cannot predict precise enough measurements for some models and
+#       predictions. Ultimately, even correct programs can yield incorrect results if the initial inputs/values are not
+#       precise enough. A math model is considered chaotic if very small changes to input lead to changes large enough
+#       to be considered random, this limits the power of computing.
+#
+# Multiple Choice questions
+#
+# 1. CS is a tool the science of information and its manipulation to solve problems, B.
+#
+# 2. Algorithms are like recipes, a set of instructions, D.
+#
+# 3. Intractable problems are problems that require too much time or memory (computing power), D.
+#
+# 4. RAM is main memory, A.
+#
+# 5. Humans need low-level machine language translated into high-level language, B.
+#
+# 6. A statement is complete command, B.
+#
+# 7. High-level language can be either 1) compiled or 2) interpreted. A compiler takes source code and compiles
+#       high-level source code to machine code BEFORE the program is executed. Once this is performed, the program/code
+#       does not need to be compiled again (translation happens once). An interpreter has to translate as the program
+#       runs, but it is more flexible across platforms since it is not dependent on the CPU. The interpreter simulates
+#       the computer "understanding" high-level source code, C.
+#
+# 8. A sequence of commands are statements for the Python interpreter to execute. Statements of a program are often
+#       placed in a function main, B.
+#
+# 9. The answer is A, self explanatory.
+#
+# 10. Parameters are synonymous with arguments in reference to a function, D.
+#
+#
+#Compare and contrast the following pairs of concepts from the chapter:
+a) Hardware vs. Software
+b) Algorithm vs. Program
+c) Programming Language vs. Natural Language
+d) High-Level Language vs. Machine Language
+e) Interpreter vs. Compiler
+f ) Syntax vs. Semantics
+2. List and explain in your own words the role of each of the five basic functional
+units of a computer depicted in Figure 1.1.
+3. Write a detailed algorithm for making a peanut butter and jelly sandwich
+(or some other everyday activity). You should assume that you are talking
+to someone who is conceptually able to do the task, but has never actually
+done it before. For example, you might be telling a young child.
+#
+
